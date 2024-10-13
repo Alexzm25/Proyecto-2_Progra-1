3b8f930f73608c8c5c05ce24bd02a5be5a85b041
Alex Zúñiga <alex.zuniga.monge@est.una.ac.cr>
2024-10-12 19:48:37 -0600

    [ADD] new classes and sound effects

--- Proyecto#2/header/GUI.h
+++ Proyecto#2/header/GUI.h
@@ -2,6 +2,11 @@
 #include <SFML/System.hpp>
 #include <SFML/Audio.hpp>
 #include <iostream>
+#define MAP_MODE 0
+#define MENU_MODE 1
+
+using namespace std;
+using namespace sf;
 
 class GUI {
 public:
--- Proyecto#2/src/GUI.cpp
+++ Proyecto#2/src/GUI.cpp
@@ -2,21 +2,22 @@
 
 void GUI::runWindow() {
 
-	sf::RenderWindow window(sf::VideoMode(1280, 720), "UNOVA ROUTES");
-	
-	sf::SoundBuffer buttonBuffer;
+	RenderWindow window(VideoMode(1280, 720), "UNOVA ROUTES");
+	window.setFramerateLimit(30);
+
+	SoundBuffer buttonBuffer;
 
 	buttonBuffer.loadFromFile("../assets/audio/audio_soundEffect.ogg");
 
-	sf::Sound buttonSound(buttonBuffer);
+	Sound buttonSound(buttonBuffer);
 
-	sf::Texture backgroundTxt;
-	sf::Texture startOptionTxt;
-	sf::Texture startOption2Txt;
-	sf::Texture startOption3Txt;
-	sf::Texture closeOptionTxt;
-	sf::Texture closeOption2Txt;
-	sf::Texture mapTxt;
+	Texture backgroundTxt;
+	Texture startOptionTxt;
+	Texture startOption2Txt;
+	Texture startOption3Txt;
+	Texture closeOptionTxt;
+	Texture closeOption2Txt;
+	Texture mapTxt;
 
 	backgroundTxt.loadFromFile("../assets/images/img_unovaRoutes.png");
 	startOptionTxt.loadFromFile("../assets/images/img_start1.png");
@@ -26,66 +27,81 @@ void GUI::runWindow() {
 	closeOption2Txt.loadFromFile("../assets/images/img_close3.png");
 	mapTxt.loadFromFile("../assets/images/img_unovaMap.jpg");
 
-	sf::Sprite backgroundSpr(backgroundTxt);
-	sf::Sprite startOptionSpr(startOptionTxt);
-	sf::Sprite startOption2Spr(startOption2Txt);
-	sf::Sprite startOption3Spr(startOption3Txt);
-	sf::Sprite closeOptionSpr(closeOptionTxt);
-	sf::Sprite closeOption2Spr(closeOption2Txt);
-	sf::Sprite mapSpr(mapTxt);
+	Sprite backgroundSpr(backgroundTxt);
+	Sprite startOptionSpr(startOptionTxt);
+	Sprite startOption2Spr(startOption2Txt);
+	Sprite startOption3Spr(startOption3Txt);
+	Sprite closeOptionSpr(closeOptionTxt);
+	Sprite closeOption2Spr(closeOption2Txt);
+	Sprite mapSpr(mapTxt);
 
 	startOptionSpr.setPosition(400, 330);
 	startOption2Spr.setPosition(400, 330);
 	startOption3Spr.setPosition(403, 330);
 
-	closeOptionSpr.setPosition(400, 480);
-	closeOption2Spr.setPosition(400, 480);
+	closeOptionSpr.setPosition(400, 520);
+	closeOption2Spr.setPosition(400, 520);
 	int posX, posY;
-	bool isMapActive = false;
+	bool isSoundPlayable = true;
+	int gameMode = 1;
+
 	while (window.isOpen()) {
-		
-		sf::Event event;
+
+		Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 				window.close();
 		}
-
 
 		window.clear();
 		
-		if (isMapActive == false) {
+		if (gameMode == MENU_MODE) {
+			if (!startOptionSpr.getGlobalBounds().contains(static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y))
+				&& !closeOptionSpr.getGlobalBounds().contains(static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y))) {
+				isSoundPlayable = true;
+			}
+			
+
 			window.draw(backgroundSpr);
 			window.draw(startOptionSpr);
 			window.draw(closeOptionSpr);
-		}
-		else window.draw(mapSpr);
 
-		
-		if (startOptionSpr.getGlobalBounds().contains(static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y)) && isMapActive == false) {
-			window.draw(startOption3Spr);
-			//buttonSound.play(); //arreglar bug de que solo uno de los dos sucede, que sucedan los 2 al mismo tiempo
-			//sf::sleep(sf::seconds(1));
-		//startOptionSpr.setColor(sf::Color(255, 255, 255, 0));
-		}
-		if (closeOptionSpr.getGlobalBounds().contains(static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y)) && isMapActive == false) {
-			window.draw(closeOption2Spr);
-		}
-		
-		if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
 
-			//CODIGO TEMPORAL, SOLO PARA VER COORDENADAS XY
-			posX = event.mouseButton.x;
-			posY = event.mouseButton.y;
-
-			std::cout << "X: " << posX << "\n";
-			std::cout << "Y: " << posY << "\n";
-			//CODIGO TEMPORAL, SOLO PARA VER COORDENADAS XY
-			if (startOption3Spr.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)) && isMapActive == false) {
-				isMapActive = true;
+			if (startOptionSpr.getGlobalBounds().contains(static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y))) {
+				if (isSoundPlayable) buttonSound.play();
+				sleep(milliseconds(100));
+				window.draw(startOption3Spr);
+				isSoundPlayable = false;
 			}
-			if (closeOption2Spr.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y)) && isMapActive == false) {
-				window.close();
+
+			if (closeOptionSpr.getGlobalBounds().contains(static_cast<float>(event.mouseMove.x), static_cast<float>(event.mouseMove.y))) {
+				if (isSoundPlayable) buttonSound.play();
+				sleep(milliseconds(100));
+				window.draw(closeOption2Spr);
+				isSoundPlayable = false;
+
 			}
+
+			if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+
+				//CODIGO TEMPORAL, SOLO PARA VER COORDENADAS XY
+				posX = event.mouseButton.x;
+				posY = event.mouseButton.y;
+
+				cout << "X: " << posX << "\n";
+				cout << "Y: " << posY << "\n";
+				//CODIGO TEMPORAL, SOLO PARA VER COORDENADAS XY
+				if (startOption3Spr.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y))) {
+					gameMode = 0;
+				}
+				if (closeOption2Spr.getGlobalBounds().contains(static_cast<float>(event.mouseButton.x), static_cast<float>(event.mouseButton.y))) {
+					window.close();
+				}
+			}
+		}
+
+		else {
+			window.draw(mapSpr);
 		}
 		window.display();
 	}
